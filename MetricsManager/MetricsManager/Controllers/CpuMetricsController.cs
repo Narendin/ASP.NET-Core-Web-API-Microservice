15cb769da0e427c3f8fa52ce620461bc6e317391
VladimirDolguev <avus93@mail.ru>
2021-09-12 15:01:43 +0300

    Урок 3
    Тестирую логгер

--- MetricsManager/MetricsManager/Controllers/CpuMetricsController.cs
+++ MetricsManager/MetricsManager/Controllers/CpuMetricsController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -13,5 +14,19 @@ namespace MetricsManager.Controllers
     [Route("api/metrics/cpu")]
     public class CpuMetricsController : BaseMetricsController
     {
+        private readonly ILogger<CpuMetricsController> _logger;
+
+        public CpuMetricsController(ILogger<CpuMetricsController> logger)
+        {
+            _logger = logger;
+            _logger.LogDebug(1, "NLog встроен в CpuMetricsController");
+        }
+
+        public override IActionResult GetMetricsFromAgent([FromRoute] int agentId, [FromRoute] TimeSpan fromTime, [FromRoute] TimeSpan toTime)
+        {
+            var result = base.GetMetricsFromAgent(agentId, fromTime, toTime);
+            _logger.LogInformation($"Получили метсрики процессора от агента {agentId}");
+            return result;
+        }
     }
 }
